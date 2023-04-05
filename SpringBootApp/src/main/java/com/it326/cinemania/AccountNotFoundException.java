16eeaab0ab4dbd17dad8e0dc297a17c50a413a75
Daniel Beyer <dbeyer180@gmail.com>
2023-04-05 14:31:40 -0500

    Removing Spring Boot files
    Removing Spring Boot files since we are no longer using them in the program.

--- SpringBootApp/src/main/java/com/it326/cinemania/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.it326.cinemania;
-
-public class AccountNotFoundException extends RuntimeException {
-    AccountNotFoundException(Long id){
-        super("Could not find account " + id);
-    }
-}

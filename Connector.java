276f820a0301ce7f472a29529ca841946d599b16
Jacobschwert <45664842+Jacobschwert@users.noreply.github.com>
2023-04-15 16:04:16 -0500

    Update Connector.java

--- Connector.java
+++ Connector.java
@@ -1,26 +1,31 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Connection;  
+import java.sql.DriverManager;  
+import java.sql.SQLException;  
+   
+public class Connector {  
 
-public class Connector {
-
-    private Connection connection;
-
-    public void connect(String dbPath) throws SQLException, ClassNotFoundException {
-        // Load the SQLite JDBC driver
-        Class.forName("org.sqlite.JDBC");
-
-        // Create a connection to the specified database file
-        connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+    public static Connection connect() {
+        Connection conn = null;  
+        try {  
+            // db parameters  
+            String url = "jdbc:sqlite:C:/Users/jacob/Desktop/IT 326/tableDB/cinemania.db";  //You need to change this filepath to reference where you have the db saved
+            // create a connection to the database  
+            conn = DriverManager.getConnection(url);  
+              
+            System.out.println("Connection to SQLite has been established.");  
+              
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        } 
+        return conn;
     }
-
-    public void disconnect() throws SQLException {
-        if (connection != null) {
-            connection.close();
+    public static void main(String[] args) {
+        Connection conn = connect();
+        try {
+            // execute your queries using the conn object
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
-
-    public Connection getConnection() {
-        return connection;
-    }
-}
+}  

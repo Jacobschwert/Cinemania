7177933fe34b102db5fdb0b8459c6237e84f3a58
Jacobschwert <45664842+Jacobschwert@users.noreply.github.com>
2023-04-25 21:04:28 -0500

    Update Queries.java

--- Queries.java
+++ Queries.java
@@ -1,19 +1,14 @@
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Queries {
-    public static ResultSet executeQuery(String sql) throws SQLException {
-        Connection conn = Connector.connect();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        return stmt.executeQuery();
-    }
-
-    public static int executeUpdate(String sql) throws SQLException {
-        Connection conn = Connector.connect();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        return stmt.executeUpdate();
+public abstract class Queries {
+    protected Connection conn;
+    
+    public Queries(Connection conn) {
+        this.conn = conn;
     }
     
+    public abstract ResultSet executeQuery(String sql) throws SQLException;
+    public abstract int executeUpdate(String sql) throws SQLException;
 }

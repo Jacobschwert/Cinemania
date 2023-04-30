6540d5e61f17119631ba92f092d68754af6aef36
OrbitalCodeRock <37189245+OrbitalCodeRock@users.noreply.github.com>
2023-04-29 23:51:35 -0500

    Almost have everything in-place needed to parse TMDB data into Movie objects, have notes for what is still needed

--- ContentFactory.java
+++ ContentFactory.java
@@ -7,35 +7,25 @@ public class ContentFactory {
         MOVIE,
     }
 
-    /*Constructor is made private to ensure that only one ContentFactory class exists at a time*/
-    private ContentFactory(){
-
-    }
-
-    /* Method that is meant to return a tv show or movie after populating its attributes accordingly. 
-     * This will likely be refactored to manipulate JSON or JSON parsed into java classes.
-    */
-    public static Content getContent(contentType cType){
-        switch(cType){
-            case TVSHOW:
-                break;
-            case MOVIE:
-                break;
-        }
-    }
-
-    /* Method that is meant to return a ContentList by making use of the TMDBCommunicator, based off of a reccomendation type. */
-    public static ContentList getContentList(ContentManager.reccomendationType rType){
+    /* Method that is meant to return a ContentList by making use of the TMDBCommunicator, based off of a Recommendation type. */
+    public static ContentList getRecommendedContentList(ContentManager.RecommendationType rType){
         switch(rType){
             case MOVIES_POPULAR:
                 break;
             case MOVIES_ACTION:
-                TMDBMovieResult[] results = TMDBCommunicator.getReccomendationListMovieResults(rType).getResults();
-                ArrayList<Content> contentList = new ArrayList<Content>(results.length);
-                for(int i = 0; i < results.length; i++){
-                    
+                TMDBMovieResultList resultList = TMDBCommunicator.getRecommendationListMovieResults(rType);
+                TMDBMovieResult[] movieResults = resultList.getResults();
+                TMDBWatchOption[] watchOptions = TMDBCommunicator.getMovieWatchOptionsArray(resultList);
+                ArrayList<Content> contentArrayList = new ArrayList<Content>(movieResults.length);
+                for(int i = 0; i < movieResults.length; i++){
+                    contentArrayList.add(getMovieFromTMDBMovieInfo(movieResults[i], watchOptions[i]));
                 }
 
+                String contentListName = "Action Movies";
+                String contentListDescription = "Popular Action Movies according to The Movie Database";
+
+                // For contentListID, I'll need a way to generate a unique ID. 
+                
             case MOVIES_ADVENTURE:
                 break;
             case MOVIES_COMEDY:
@@ -45,8 +35,34 @@ public class ContentFactory {
         }
     }
 
-    private Content getContentFromTMDBMovieResult(TMDBMovieResult result){
-        
+    // A helper method for creating a Movie object from TMDB information.
+    private static Movie getMovieFromTMDBMovieInfo(TMDBMovieResult result, TMDBWatchOption movieWatchOption){
+        int contentId = result.getId(); 
+        String contentName = result.getTitle();
+        String contentDescription = result.getOverview();
+        int[] contentGenreIds = result.getGenre_ids();
+        String[] genreNames = TMDBCommunicator.getMovieGenresByTMDBID(contentGenreIds);
+
+        TMDBCountryWatchOptionList usList = movieWatchOption.getResult().getUSOptionList();
+        String[] buyProviders = getProviderNamesFromProviderInfoArray(usList.getBuyOptions());
+        String[] rentalProviders = getProviderNamesFromProviderInfoArray(usList.getRentOptions());
+        String[] flatrateProviders = getProviderNamesFromProviderInfoArray(usList.getFlatrateOptions());
+
+        // To get the contentRating of a content object, I'll need to ping the Cinemania database and
+        // and take the average rating of all reviews for that piece of content. If no ratings exist, then I will have it be null (or thats an idea).
+
+        // For a review list, I'll need to ping the Feedback factory, which will then look through the cinemania database use information stored there
+        // to bring back a list of review objects representing reviews written from that piece of content. If there are no reviews,
+        // then I'll have the review list be either null or empty.
+
+    }
+
+    private static String[] getProviderNamesFromProviderInfoArray(TMDBWatchOptionProviderInfo[] providerInfos){
+        String[] returnStrings = new String[providerInfos.length];
+        for(int i = 0; i < providerInfos.length; i++){
+            returnStrings[i] = providerInfos[i].getProviderName();
+        }
+        return returnStrings;
     }
 
 }

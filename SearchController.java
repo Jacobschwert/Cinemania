e5bc308b46060542eb2fcef49c0cfdfebc017fcc
AndrewR115 <77765261+AndrewR115@users.noreply.github.com>
2023-05-02 16:26:37 -0500

    Add files via upload

---
+++ SearchController.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SearchController {
+    
+    public void viewReccomendations() {
+        System.out.println("Viewing Reccomendations");
+    }
+
+    public void searchUsers() {
+        System.out.println("Searching Users");
+    }
+
+    public void searchContent() {
+        System.out.println("Searching Content");
+    }
+}

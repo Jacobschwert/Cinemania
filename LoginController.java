b89c7945f037ab8baadfd0328ca29113affa106e
Jacobschwert <45664842+Jacobschwert@users.noreply.github.com>
2023-05-04 22:02:23 -0500

    Update LoginController.java

--- LoginController.java
+++ LoginController.java
@@ -46,4 +46,52 @@ public class LoginController {
         Account signupAcct = new Account(uName, email, desc, password);
         return signupAcct;
     }
+
+    public void editAccount(Account accountEdit){//Do stuff here
+        String uName;
+        String password;
+        String email;
+        String desc;
+        String tempToDelete;
+        Boolean toDelete = false;
+        
+        Scanner editInfo = new Scanner(System.in);
+
+        System.out.println("Enter 'n' if you don't want to change a value.");
+        System.out.print("Enter Username: ");
+        uName = editInfo.nextLine();
+        if(uName.equals("n")){
+            uName = accountEdit.getUName();
+        }
+        System.out.print("Enter Password: ");
+        password = editInfo.nextLine();
+        if(password.equals("n")){
+            password = accountEdit.getPassword();
+        }
+        System.out.print("Enter Email: ");
+        email = editInfo.nextLine();
+        if(email.equals("n")){
+            email = accountEdit.getEmail();
+        }
+        System.out.print("Enter Description: ");
+        desc = editInfo.nextLine();
+        if(desc.equals("n")){
+            desc = accountEdit.getDescription();
+        }
+        System.out.print("Do you want to delete your account(y/n)? ");
+        tempToDelete = editInfo.nextLine();
+        if(tempToDelete.equals("y")){
+            toDelete = true;
+        }
+        editInfo.close();
+
+        accountEdit.editAccount(uName, email, desc, password, toDelete);
+    }
+
+    public static void main(String args[]){
+        // System.out.println(test2.getAccountNumber());
+        // LoginController lc = new LoginController();
+        // Account test;
+        // test = lc.login();
+    }
 }

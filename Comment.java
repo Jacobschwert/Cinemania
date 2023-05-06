865a6c6bfa443a0fffb333555fb120af284392ac
Corey <124395256+Cudiwrld@users.noreply.github.com>
2023-05-05 21:08:37 -0500

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -1,14 +1,18 @@
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Comment extends Feedback{
     private String text;
     private int likes;
     private Account feedbackAuthor;
+    private ArrayList<Comment> commentList;
     private int feedbackID;
-    private Review reviewTarget;
+    private Review commentTarget;
     private int targetID;
 
     private SqliteConnector db = new SqliteConnector();
@@ -16,17 +20,16 @@ public class Comment extends Feedback{
     private SqliteQueries query = new SqliteQueries(conn);
     private String queryString;
 
-
-    public Comment(String text, Review reviewTarget) throws IllegalArgumentException {
+    //Creates a comment on a specific Review
+    public Comment(String text, Review commentTarget, Account feedbackAuthor) throws IllegalArgumentException {
         if (text.length() > 150) {
             throw new IllegalArgumentException("Comment cannot exceed 150 characters");
         }
         this.text = text;
         this.likes = 0;
-        this.reviewTarget = reviewTarget;
-        this.targetID = reviewTarget.getFeedbackID();
-        this.feedbackAuthor = reviewTarget.getFeedbackAuthor();
-        queryString = "INSERT INTO comment(text, likes, review_id, author_id) VALUES('" + text + "', " + likes + ", " + reviewTarget.getFeedbackID() + ", " + feedbackAuthor.getAccountNumber() + ");";
+        targetID = commentTarget.getFeedbackID();
+        this.feedbackAuthor = feedbackAuthor;
+        queryString = "INSERT INTO comment(text, likes, review_ID, author_id) VALUES('" + text + "', " + likes + ", " + commentTarget.getFeedbackID() + ", " + feedbackAuthor.getAccountNumber() + ");";
         try {
             query.executeUpdate(queryString);
         } catch(SQLException e) {
@@ -34,7 +37,7 @@ public class Comment extends Feedback{
         }
     }
     
-
+    //Adds a like to a comment
     public void addLike() {
         this.likes++;
         queryString = "UPDATE comment SET likes = " + likes + " WHERE feedback_id = " + feedbackID + ";";
@@ -45,7 +48,7 @@ public class Comment extends Feedback{
             e.printStackTrace();
         }
     }    
-
+    //Removes a like from a comment
     public void removeLike() {
         if (this.likes > 0) {
             this.likes--;
@@ -58,17 +61,15 @@ public class Comment extends Feedback{
             }
         }
     }
-    
-    
+    //Edits a comment to have new text
     public void editFeedback() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 'Y' to confirm you want to edit the comment or 'N' to cancel");
-        String answer = scan.nextLine();
-        scan.close();
+        String answer = scanner.nextLine();
         if(answer.equalsIgnoreCase("Y"))
         {
             System.out.println("Enter your new comment text: ");
-            String newText = scan.nextLine();
+            String newText = scanner.nextLine();
             this.text = newText;
             queryString = "UPDATE comment SET text = '" + newText + "' WHERE feedback_id = " + feedbackID + ";";
             try{
@@ -81,15 +82,17 @@ public class Comment extends Feedback{
         else{
             System.out.println("Comment edit has been cancelled");
         }
-
+        scanner.close();
     }
-
+    //Deletes a comment
     public void deleteFeedback() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 'Y' to confirm that you want to delete the comment: ");
         String response = scanner.nextLine();
         scanner.close();
         if(response.equalsIgnoreCase("Y")){
+            setFeedbackSummary(null);
+            setLikes(0);
             queryString = "DELETE FROM comment WHERE feedback_id = " + feedbackID + ";";
             try{
                 query.executeUpdate(queryString);
@@ -101,7 +104,35 @@ public class Comment extends Feedback{
             System.out.println("Comment deletion has been cancelled.");
         }
     }
-    
+
+    public void generateID() {
+        Random rand = new Random();
+        Boolean moveOn = false;
+        int number = 0;
+        while (moveOn == false){ //Generate accountID
+            number = rand.nextInt(88888) + 11111; //This gives a range of 11111 - 99999
+            queryString = "SELECT feedbackID FROM account WHERE feedbackID = " + number + ";" ;
+            ResultSet rs;
+            try{
+                rs = query.executeQuery(queryString);
+                if (!rs.next()) {
+                    this.feedbackID = number;
+                    moveOn = true;
+                }
+            } catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+     //Method adds a comment that gets created to its list of comments
+     public void addComment(Comment comment) {
+        this.commentList.add(comment);
+    }
+
+    //Method lists all comments left on a review
+    public ArrayList<Comment> getCommentList() {
+        return commentList;
+    }
 
     // Getters and setters
 
@@ -142,7 +173,9 @@ public class Comment extends Feedback{
     }
 
     public String toString() {
-        return text;
+        return "Comment by " + feedbackAuthor.getUName() + ": " + text + " (Likes: " + likes + ")";
+
+        
     }
 
 }
